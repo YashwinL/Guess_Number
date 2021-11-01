0f403fdfce11480ebfbfc07ef0f939c1c7d20d42
YashwinL <yashwinlakshmipathy@gmail.com>
2021-11-01 19:39:55 +0530

    Completed Code

--- script.js
+++ script.js
@@ -7,30 +7,35 @@ document.querySelector('.again').addEventListener('click', function () {
   ran_num = Math.trunc(Math.random() * 20);
   document.querySelector('.number').textContent = '?';
 });
+function display(message1) {
+  document.querySelector('.message').textContent = message1;
+}
 
 let score = 20;
+let highscore = 0;
 let ran_num = Math.trunc(Math.random() * 20);
 document.querySelector('.check').addEventListener('click', function () {
   const typed_num = Number(document.querySelector('.guess').value);
   if (!typed_num) {
-    document.querySelector('.message').textContent = `🚫 no value entered`;
+    display('🚫 no value entered');
   } else if (score >= 1) {
     if (typed_num === ran_num) {
       document.querySelector('.number').textContent = ran_num;
-      document.querySelector('.message').textContent = `✔️ correct number`;
+      display('✔️ correct number');
       score++;
       document.querySelector('body').style.backgroundColor = ' #60b347';
       document.querySelector('.score').textContent = score;
-    } else if (typed_num > ran_num) {
-      document.querySelector('.message').textContent = `❌ Too High`;
-      score--;
-      document.querySelector('.score').textContent = score;
-    } else if (typed_num < ran_num) {
-      document.querySelector('.message').textContent = `❌ Too Less`;
+      if (score > highscore) {
+        highscore = score;
+        document.querySelector('.highscore').textContent = highscore;
+      }
+    } else if (typed_num !== ran_num) {
+      document.querySelector('.message').textContent =
+        typed_num > ran_num ? '❌ Too High' : '❌ Too low';
       score--;
       document.querySelector('.score').textContent = score;
     }
   } else if (score < 1) {
-    document.querySelector('.message').textContent = `you lost`;
+    display(' 👎 you lost');
   }
 });
